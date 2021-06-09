cbb11698e8bf338697b0241a672e29d790aa6aea
Novanda Ahsan <novandaahsan1@gmail.com>
2021-06-09 20:11:39 +0700

    web: fix - expected server HTML contain a matching...

--- web/src/modules/auth/WaitForAuth.tsx
+++ web/src/modules/auth/WaitForAuth.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useTokenStore } from "./useTokenStore";
 import { useVerifyLoggedIn } from "./useVerifyLoggedIn";
 
@@ -7,7 +7,12 @@ interface WaitForWsAndAuthProps {}
 export const WaitForAuth: React.FC<WaitForWsAndAuthProps> = ({ children }) => {
   useVerifyLoggedIn();
   const hasTokens = useTokenStore((s) => !!(s.accessToken && s.refreshToken));
+  const [loggedIn, setLoggedIn] = useState(false);
 
-  if (hasTokens) return <>{children}</>;
+  useEffect(() => {
+    if (hasTokens) setLoggedIn(hasTokens);
+  }, [hasTokens]);
+
+  if (loggedIn) return <>{children}</>;
   return <></>;
 };
