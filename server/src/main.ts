c49ec6864ea89b2350405a6dbb3e28a45fb9af05
Novanda Ahsan <novandaahsan1@gmail.com>
2021-06-14 15:01:05 +0700

    allowing fe: in God we trust :)

--- server/src/main.ts
+++ server/src/main.ts
@@ -3,9 +3,12 @@ import { AppModule } from './app.module';
 import * as cookieParser from 'cookie-parser';
 import { GraphqlAuthGuard } from './modules/auth/guards/graphql-auth.guard';
 import * as cors from 'cors';
+import { NestExpressApplication } from '@nestjs/platform-express';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  app.set('trust proxy', 1);
+  app.set();
   app.use(
     cors({
       credentials: true,
