7ebd72459a98cc65b72b3ed6dca75fa34e9b3130
Novanda Ahsan <novandaahsan1@gmail.com>
2021-06-14 12:47:04 +0700

    server: allowing fe

--- server/src/main.ts
+++ server/src/main.ts
@@ -2,17 +2,24 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as cookieParser from 'cookie-parser';
 import { GraphqlAuthGuard } from './modules/auth/guards/graphql-auth.guard';
+import * as cors from 'cors';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.use(cookieParser());
+  app.use(
+    cors({
+      credentials: true,
+      origin: ['https://teamhouse.vercel.app', 'http://localhost:3000'],
+    }),
+  );
 
-  app.enableCors({
-    credentials: true,
-    origin: ['https://teamhouse.vercel.app', 'http://localhost:3000'],
-  });
+  // app.enableCors({
+  //   credentials: true,
+  //   origin: ['https://teamhouse.vercel.app', 'http://localhost:3000'],
+  // });
 
   app.useGlobalGuards(new GraphqlAuthGuard());
+  app.use(cookieParser());
 
   const PORT = process.env.PORT || 80;
   await app.listen(PORT, () =>
--- web/src/utils/withApollo.ts
+++ web/src/utils/withApollo.ts
@@ -1,17 +1,21 @@
 import { createWithApollo } from "./createWithApollo";
-import { ApolloClient, InMemoryCache } from "@apollo/client";
+import { ApolloClient, HttpLink, InMemoryCache } from "@apollo/client";
 import { NextPageContext } from "next";
+import { isServer } from "./isServer";
 
 const createClient = (ctx: NextPageContext) =>
   new ApolloClient({
-    uri: process.env.NEXT_PUBLIC_API_URL as string,
-    credentials: "include",
-    headers: {
-      cookie:
-        (typeof window === "undefined"
-          ? ctx?.req?.headers.cookie
-          : undefined) || "",
-    },
+    ssrMode: isServer,
+    link: new HttpLink({
+      uri: process.env.NEXT_PUBLIC_API_URL as string,
+      credentials: "include",
+      headers: {
+        cookie:
+          (typeof window === "undefined"
+            ? ctx?.req?.headers.cookie
+            : "web") || "not",
+      },
+    }),
     cache: new InMemoryCache({}),
   });
 
