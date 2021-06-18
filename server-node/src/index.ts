59ebb5283dc753113c83208ecc8bcbec1e00cbec
Novanda Ahsan <nathoilla@indo-soft.com>
2021-06-18 12:57:43 +0700

    server-node: change fallback port

--- server-node/src/index.ts
+++ server-node/src/index.ts
@@ -19,7 +19,7 @@ import './services/auth/google';
 
 dotenv.config();
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 
 const main = async () => {
   const app: Express = express();
