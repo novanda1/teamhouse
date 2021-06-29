9789e525a520d9bdb83776d6d329939b7b9c3277
Novanda Ahsan <novandaahsan1@gmail.com>
2021-06-29 12:36:43 +0700

    seno: subscription worked

--- server-node/src/index.ts
+++ server-node/src/index.ts
@@ -1,90 +1,37 @@
-import { ApolloServer } from 'apollo-server-express';
-import cookieParser from 'cookie-parser';
-import cors from 'cors';
+import chalk from 'chalk';
 import dotenv from 'dotenv';
-import express, { Express } from 'express';
-import helmet from 'helmet';
-import { connect, connection } from 'mongoose';
-import passport from 'passport';
+import { ApolloServer } from 'apollo-server-express';
 import 'reflect-metadata';
-import { buildSchema } from 'type-graphql';
-import { chatFeature } from './features/chat/chat';
-import { ChatTeamResolver } from './resolvers/chatTeamResolver';
-import { HelloResolver } from './resolvers/hello';
-import { SubscriptionResolver } from './resolvers/SubscriptionResolver';
-import { TeamRefResolver } from './resolvers/teamRefResolver';
-import { TeamResolver } from './resolvers/teamResolver';
-import { UserResolver } from './resolvers/userResolver';
-import router from './routes';
-import './services/auth/google';
-
-dotenv.config();
+import http from 'http';
+import ServerConfig from './config/server.config';
 
 const PORT = process.env.PORT || 80;
 
+dotenv.config();
+
 const main = async () => {
-  const http = require('http');
-  const app: Express = express();
-  const server = http.createServer(app);
+  await ServerConfig.connectDB();
+  ServerConfig.getExpress()
+    .then(({ appExpress, schema }) => {
+      const server = new ApolloServer({
+        schema,
+        context: (context) => context,
+        subscriptions: {
+          onConnect() {},
+          onDisconnect() {},
+        },
+      });
+      server.applyMiddleware({ app: appExpress, path: '/graphql' });
+      const httpServer = http.createServer(appExpress);
+      server.installSubscriptionHandlers(httpServer);
 
-  /** db */
-  await connect(process.env.DATABASE_URL, {
-    useNewUrlParser: true,
-    useFindAndModify: false,
-    useUnifiedTopology: true,
-    useCreateIndex: true,
-  });
-
-  const db = connection;
-  db.on('error', console.error.bind(console, 'MongoDB connection error:'));
-
-  app.use(
-    cors({
-      origin: process.env.CORS_ORIGIN,
-      credentials: true,
-    }),
-  );
-
-  /** apollo */
-  const apolloServer = new ApolloServer({
-    // subscriptions: {
-    //   path: '/chatTeam',
-    // },
-    schema: await buildSchema({
-      resolvers: [
-        HelloResolver,
-        UserResolver,
-        TeamResolver,
-        TeamRefResolver,
-        ChatTeamResolver,
-        SubscriptionResolver,
-      ],
-      validate: false,
-    }),
-    context: ({ req, res }) => ({
-      req,
-      res,
-    }),
-  });
-
-  /** app */
-  app.use(helmet());
-  app.set('trust proxy', 1);
-  app.use(express.json());
-  app.use(cookieParser());
-  // if (!__prod__) app.use(session(sessionOpt));
-  app.use(passport.initialize());
-  // app.use(passport.session());
-
-  app.use(router);
-  apolloServer.applyMiddleware({
-    app,
-    cors: false,
-  });
-
-  chatFeature(server);
-
-  server.listen(PORT, () => console.log(`Running on ${PORT} ⚡`));
+      httpServer.listen(Number(PORT), () => {
+        console.log(chalk.green(`Server started on port ${PORT}`));
+      });
+    })
+    .catch((_) => {
+      console.log(chalk.red(`Unable to start server on port ${PORT}`));
+    });
 };
 
 main();
