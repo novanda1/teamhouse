a9f88223228042b47cac0abd3b4a3ac214be357e
Novanda Ahsan <novandaahsan1@gmail.com>
2021-06-30 09:37:01 +0700

    seno: remove dotenv on index.js

--- server-node/src/index.ts
+++ server-node/src/index.ts
@@ -1,8 +1,5 @@
-import dotenv from 'dotenv';
 import 'reflect-metadata';
 import ServerConfig from './lib/config/server.config';
-
-dotenv.config();
 
 const main = async () => {
   await ServerConfig.connectDB();
