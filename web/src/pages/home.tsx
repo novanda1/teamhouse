e915b60511afed55ae68aad353648b95ed44355b
Novanda Ahsan <novandaahsan1@gmail.com>
2021-07-02 20:32:52 +0700

    remomving console

--- web/src/pages/home.tsx
+++ web/src/pages/home.tsx
@@ -1,37 +1,16 @@
 import React, { useEffect } from "react";
-import {
-  useNotifSubscription,
-  usePushNotifMutation,
-} from "../generated/graphql";
 import { MainScreen } from "../modules/common/MainScreen";
-import { usePanelStore, IPanelStore } from "../modules/common/usePanelStore";
+import { IPanelStore, usePanelStore } from "../modules/common/usePanelStore";
 import { withApollo } from "../utils/withApollo";
 
 const Home = () => {
   const panel = usePanelStore();
-  const s = useNotifSubscription();
-  const [p] = usePushNotifMutation();
 
-  useEffect(() => {
-    console.log(`s`, s);
-  }, [s]);
   useEffect(() => {
     panel.set((s: IPanelStore) => void (s.mainPanel = "idle"));
   }, []);
 
-  return (
-    <>
-      {JSON.stringify(s)}
-      <button
-        onClick={() => {
-          p({ variables: { message: "asd" } });
-        }}
-      >
-        push
-      </button>
-      <MainScreen />
-    </>
-  );
+  return <MainScreen />;
 };
 
 export default withApollo()(Home);
