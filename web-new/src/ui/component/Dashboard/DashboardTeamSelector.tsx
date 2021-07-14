ae24779f1545e3ccfc4696af46eca623f967f243
Novanda Ahsan <novandaahsan1@gmail.com>
2021-07-15 06:47:34 +0700

    wen: dashboard UI

--- web-new/src/ui/component/Dashboard/DashboardTeamSelector.tsx
+++ web-new/src/ui/component/Dashboard/DashboardTeamSelector.tsx
@@ -10,14 +10,14 @@ export const DashboardTeamSelector: React.FC<DashboardTeamSelectorProps> =
       <Button
         backgroundColor="white"
         shadow="sm"
-        py="2"
+        py="2.5"
         height="max-content"
         display="flex"
         justifyContent="flex-start"
         rounded="xl"
         position="relative"
       >
-        <Avatar name="Sejalur Studio" size="sm" mr="2" />
+        <Avatar name="Sejalur Studio" size="sm" mr="3" />
         <Text mr="3">Some Studio</Text>
         <Text color="text.dimmed" position="absolute" right="3">
           <RiArrowDropUpLine size="1.5em" style={{ marginBottom: "-10px" }} />
