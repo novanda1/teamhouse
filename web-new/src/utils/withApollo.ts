af46b1ed52bab92b47fd5759d698fae13fe71db8
Novanda Ahsan <novandaahsan1@gmail.com>
2021-07-19 15:37:26 +0700

    wen: setup apollo graphql client

--- web-new/src/utils/withApollo.ts
+++ web-new/src/utils/withApollo.ts
@@ -2,14 +2,14 @@ import { ApolloClient, HttpLink, InMemoryCache, split } from "@apollo/client";
 import { WebSocketLink } from "apollo-link-ws";
 import { getMainDefinition } from "apollo-utilities";
 import { NextPageContext } from "next";
-import { useGetTokenFromUrl } from "../hooks/useGetTokenFromUrl";
-import { ITokenStore, useTokenStore } from "../modules/auth/useTokenStore";
+import { IAuthStore, useAuthStore } from "../modules/auth/useAuthStore";
+import { useGetTokenFromUrl } from "../modules/auth/useGetTokenFromUrl";
 import { createWithApollo } from "./createWithApollo";
 import { isServer } from "./isServer";
 
 const createClient = (ctx: NextPageContext) => {
   const token =
-    useGetTokenFromUrl() || useTokenStore((s: ITokenStore) => s.accessToken);
+    useGetTokenFromUrl() || useAuthStore((s: IAuthStore) => s.accessToken);
 
   const httpLink = new HttpLink({
     uri: process.env.NEXT_PUBLIC_API_URL,
