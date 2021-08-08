ac892e11f446515259e5c4b3ea9e60123a397961
Novanda Ahsan <novandaahsan1@gmail.com>
2021-08-08 10:59:32 +0700

    add relational model between team and project

--- server-node/src/schema/project.schema.ts
+++ server-node/src/schema/project.schema.ts
@@ -19,6 +19,25 @@ export class Project {
   description!: string;
 }
 
+@ObjectType()
+@Schema()
+export class ProjectTeamRef {
+  @Field(() => String)
+  _id!: string;
+  @Field(() => String)
+  team_id!: string;
+  @Field(() => String)
+  project_id!: string;
+}
+
+export type ProjectTeamRefDocument = ProjectTeamRef & Document;
+export const ProjectTeamRefSchema =
+  SchemaFactory.createForClass(ProjectTeamRef);
+export const ProjectTeamRefModel: Model<ProjectTeamRefDocument> = model(
+  ProjectTeamRef.name,
+  ProjectTeamRefSchema,
+);
+
 export type ProjectDocument = Project & Document;
 export const ProjectSchema = SchemaFactory.createForClass(Project);
 export const ProjectModel: Model<ProjectDocument> = model(
