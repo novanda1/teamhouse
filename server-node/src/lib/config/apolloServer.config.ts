8561e98caa9365a5a2a8942c9dabeba5b7f02f28
Novanda Ahsan <novandaahsan1@gmail.com>
2021-12-01 17:02:42 +0700

    learn ws for messaging

--- server-node/src/lib/config/apolloServer.config.ts
+++ server-node/src/lib/config/apolloServer.config.ts
@@ -1,6 +1,7 @@
 import { ApolloServer } from 'apollo-server-express';
 import chalk from 'chalk';
 import http from 'http';
+import * as WebSocket from 'ws';
 import ServerConfig from './server.config';
 
 export class ApolloServerConfig extends ServerConfig {
@@ -14,6 +15,21 @@ export class ApolloServerConfig extends ServerConfig {
       });
 
       apolloServer.applyMiddleware({ app: appExpress });
+
+      const wss = new WebSocket.Server({ server: httpServer });
+
+      wss.on('connection', (ws: WebSocket) => {
+        ws.on('message', function incoming(data) {
+          wss.clients.forEach(function each(client) {
+            if (client.readyState === WebSocket.OPEN) {
+              console.log(`Received message ${data.toString()} from user ${JSON.stringify(client)}`);
+              client.send(data.toString());
+            }
+          });
+        });
+
+        ws.send('Hi there, I am a WebSocket server');
+      });
 
       httpServer.listen(Number(PORT), () => {
         console.log(chalk.green(`Server started on port ${PORT}`));
