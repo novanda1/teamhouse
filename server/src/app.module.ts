bdbb19be73dc647f31ef8b85b5ffc5eda0782d16
Novanda Ahsan <novandaahsan1@gmail.com>
2021-12-03 05:34:26 +0700

    database connection

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -4,11 +4,12 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TeamModule } from './modules/team/team.module';
 import { UserModule } from './modules/user/user.module';
+import { DatabaseModule } from './database/database.module';
 
 @Module({
   imports: [ GraphQLModule.forRoot({
     autoSchemaFile: true
-  }), TeamModule, UserModule,],
+  }), TeamModule, UserModule, DatabaseModule,],
   controllers: [AppController],
   providers: [AppService],
 })
