65e1763d0249f65d623ae8d051782614dffb13bc
Novanda Ahsan <novandaahsan1@gmail.com>
2021-12-03 06:28:30 +0700

    add google auth

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -5,11 +5,12 @@ import { AppService } from './app.service';
 import { TeamModule } from './modules/team/team.module';
 import { UserModule } from './modules/user/user.module';
 import { DatabaseModule } from './database/database.module';
+import { GoogleModule } from './google/google.module';
 
 @Module({
   imports: [ GraphQLModule.forRoot({
     autoSchemaFile: true
-  }), TeamModule, UserModule, DatabaseModule,],
+  }), TeamModule, UserModule, DatabaseModule, GoogleModule,],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ server/src/google/google.module.ts
@@ -0,0 +1,10 @@
+import { Module } from '@nestjs/common';
+import { GoogleController } from './google.controller';
+import { GoogleService } from './google.service';
+import { GoogleStrategy } from './google.strategy';
+
+@Module({
+  controllers: [GoogleController],
+  providers: [GoogleService, GoogleStrategy]
+})
+export class GoogleModule { }
